c9c5857a7c8f90089f2ac72a7551163162a59fda
Zaki udding <zakiuddin321@gmail.com>
2021-02-01 18:49:03 +0530

    added files for making colums free

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var table = $('#example').DataTable( {
+        scrollY:        "300px",
+        scrollX:        true,
+        scrollCollapse: true,
+        paging:         false,
+        fixedColumns:   {
+            leftColumns: 2,
+            rightColumns: 1
+        }
+    } );
+} );
