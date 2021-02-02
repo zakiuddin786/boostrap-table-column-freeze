e7583ba3611179d2d6dda2879b322751f550f192
Zaki udding <zakiuddin321@gmail.com>
2021-02-02 15:24:05 +0530

    added styling

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
     var table = $('#example').DataTable( {
-        scrollY:        "300px",
+        // scrollY:        "300px",
         scrollX:        true,
         scrollCollapse: true,
-        paging:         false,
+        paging:         true,
         fixedColumns:   {
             leftColumns: 2,
             rightColumns: 1
         }
     } );
 } );
+
+
